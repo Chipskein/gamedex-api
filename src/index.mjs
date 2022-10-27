b25f6e64f89c5b6e4241caad5d094d207b96be5b
Chipskein <abfn0905@gmail.com>
2022-10-27 17:39:51 -0300

    arrumando

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 config();
 import { CreateAppInstace } from './app.mjs';
-import {CreateSequelizeInstance } from './database/orm/sequelize.mjs'
+//import {CreateSequelizeInstance } from './database/orm/sequelize.mjs'
 const PORT=process.env.PORT || 3001;  
-const database=CreateSequelizeInstance(process.env.ENVIROMENT)
-const app = CreateAppInstace(database)
+//const database=CreateSequelizeInstance()
+const app = CreateAppInstace()
 app.listen(PORT,() => console.log(`Listing on PORT ${PORT}`))
