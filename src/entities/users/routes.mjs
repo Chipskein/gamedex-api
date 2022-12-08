c81009d1d4db19abf0e8dd9cdeba63307a3a13a9
Chipskein <abfn0905@gmail.com>
2022-12-08 07:23:19 -0300

    fix:createUser

--- src/entities/users/routes.mjs
+++ src/entities/users/routes.mjs
@@ -3,7 +3,7 @@ import { verifyToken } from "../../middlewares/auth.mjs";
 import { upload } from "../../utils/image.mjs";
 import {CreateUser,AuthUser, UpdateUser,DeleteUser, GetUser, GetUsers, SearchUsers} from './controller.mjs'
 let RouterUsers=Router()
-RouterUsers.post('/',CreateUser);
+RouterUsers.post('/',upload.single('img'),CreateUser);
 RouterUsers.post('/auth',AuthUser);
 RouterUsers.put('/',verifyToken,upload.single('img'),UpdateUser);
 RouterUsers.delete('/',verifyToken,DeleteUser);
